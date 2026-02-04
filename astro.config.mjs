9452ddffc2c7045dbe6cb7c1f2329879d130ef97
liuzezheng <dongbeibubai@163.com>
2026-02-04 09:40:22 +0800

    formula rendering error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   adapter: cloudflare(),
 });
