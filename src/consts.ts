d71b1de0e8432d1ea2a91d73598e132f844a0e29
liuzezheng <dongbeibubai@163.com>
2026-05-28 14:24:14 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   NAME: "Subaru's Blog",
   EMAIL: "dongbeibubai@163.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_WORKS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
